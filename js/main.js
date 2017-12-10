c3cd7008587d6a90073f5e679f3a72c2ebea0a4d
Cristian Vargas <cristianv975@gmail.com>
2017-12-10 07:52:00 -0500

    subnav

--- js/main.js
+++ js/main.js
@@ -9,10 +9,27 @@ button.addEventListener('click', () => {
 })
 
 // nav
-
 const navButton = document.getElementById('nav__icon')
 const navContainer = document.getElementById('nav__container')
 
 navButton.addEventListener('click', () => {
   navContainer.classList.toggle('nav__container--show')
 })
+
+// subnav
+const navItems = document.getElementsByClassName('nav__link')
+const subNavs = document.getElementsByClassName('subnav')
+
+for (let i = 0; i < navItems.length; i++) {
+  navItems[i].addEventListener('click', function (e) {
+    if (e.target.nextSibling.nextSibling.classList.contains('subnav--show')) {
+      e.target.nextSibling.nextSibling.classList.remove('subnav--show')
+    } else {
+      for (let i = 0; i < subNavs.length; i++) {
+        subNavs[i].classList.remove('subnav--show')
+        e.target.nextSibling.nextSibling.classList.add('subnav--show')
+      }
+    }
+
+  })
+}
