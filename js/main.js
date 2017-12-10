59d6330f6e0c931a2a9add9aff999debcf7e4547
Cristian Vargas <cristianv975@gmail.com>
2017-12-10 06:27:14 -0500

    nav responsive

--- js/main.js
+++ js/main.js
@@ -1,9 +1,18 @@
 'use strict'
 
-// nav
+// search
 const button = document.getElementById('search__button')
 const search = document.getElementById('search')
 
 button.addEventListener('click', () => {
   search.classList.toggle('search--show')
+})
+
+// nav
+
+const navButton = document.getElementById('nav__icon')
+const navContainer = document.getElementById('nav__container')
+
+navButton.addEventListener('click', () => {
+  navContainer.classList.toggle('nav__container--show')
 })
